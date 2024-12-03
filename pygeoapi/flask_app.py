8f9b17df3af343b22221a4b832b6cb01c78d8c23
Tik <wai.tik.chan@ut.ee>
2024-12-03 16:44:53 +0200

    static_url

--- pygeoapi/flask_app.py
+++ pygeoapi/flask_app.py
@@ -66,7 +66,7 @@ STATIC_FOLDER = 'static'
 if 'templates' in CONFIG['server']:
     STATIC_FOLDER = CONFIG['server']['templates'].get('static', 'static')
 
-APP = Flask(__name__, static_folder=STATIC_FOLDER, static_url_path='/static')
+APP = Flask(__name__, static_folder=STATIC_FOLDER, static_url_path=static_url)
 APP.url_map.strict_slashes = API_RULES.strict_slashes
 
 BLUEPRINT = Blueprint(
